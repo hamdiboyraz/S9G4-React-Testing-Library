74c3cc8140f859815f4fab2d02933b1286058e25
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-03-26 21:14:03 +0300

    Complete project

--- frontend/components/IletisimFormu.test.js
+++ frontend/components/IletisimFormu.test.js
@@ -1,41 +1,110 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event';
-import IletisimFormu from './IletisimFormu';
+import React from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import IletisimFormu from "./IletisimFormu";
 
-test('hata olmadan render ediliyor', () => {
+describe("IletisimFormu component", () => {
+  beforeEach(() => {
+    render(<IletisimFormu />);
+  });
 
-});
+  test("hata olmadan render ediliyor", () => {});
 
-test('iletişim formu headerı render ediliyor', () => {
+  test("iletişim formu headerı render ediliyor", () => {
+    const header = screen.getByText("İletişim Formu");
+    expect(header).toBeInTheDocument();
+    expect(header).toBeVisible();
+    expect(header).toBeTruthy();
+  });
 
-});
+  test("kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.", async () => {
+    const adInput = screen.getByLabelText("Ad*");
+    fireEvent.change(adInput, { target: { value: "asd" } });
+    // userEvent.type(adInput, "ilha");
+    const errorMessage = screen.getByTestId("error");
 
-test('kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.', async () => {
+    expect(errorMessage).toHaveTextContent("ad en az 5 karakter olmalıdır.");
+    expect(errorMessage).toBeInTheDocument();
+  });
 
-});
+  test("kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.", async () => {
+    const btnSubmit = screen.getByText("Gönder");
+    userEvent.click(btnSubmit);
+    const errorMessages = screen.getAllByTestId("error");
+    expect(errorMessages).toHaveLength(3);
+  });
 
-test('kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.', async () => {
+  test("kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.", async () => {
+    const adInput = screen.getByLabelText("Ad*");
+    const soyadInput = screen.getByLabelText("Soyad*");
 
-});
+    fireEvent.change(adInput, { target: { value: "asdasd" } });
+    fireEvent.change(soyadInput, { target: { value: "asd" } });
+    fireEvent.submit(screen.getByRole("button"));
 
-test('kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.', async () => {
+    const errorMessage = screen.getByTestId("error");
+    expect(errorMessage).toHaveTextContent(
+      "email geçerli bir email adresi olmalıdır."
+    );
+  });
 
-});
+  test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {
+    const emailInput = screen.getByLabelText("Email*");
 
-test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {
+    userEvent.type(emailInput, "abc");
 
-});
+    // fireEvent.change(emailInput, { target: { value: "asd" } });
 
-test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {
+    const errorMessage = screen.getByTestId("error");
+    expect(errorMessage).toHaveTextContent(
+      "email geçerli bir email adresi olmalıdır."
+    );
+  });
 
-});
+  test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {
+    const adInput = screen.getByLabelText("Ad*");
+    const emailInput = screen.getByLabelText("Email*");
+    const btnSubmit = screen.getByText("Gönder");
 
-test('ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.', async () => {
+    userEvent.type(adInput, "asdasd");
+    userEvent.type(emailInput, "asd@asd.com");
+    userEvent.click(btnSubmit);
 
-});
+    const errorMessage = screen.getByTestId("error");
+    expect(errorMessage).toHaveTextContent("soyad gereklidir.");
+  });
 
-test('form gönderildiğinde girilen tüm değerler render ediliyor.', async () => {
+  test("ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.", async () => {
+    const adInput = screen.getByLabelText("Ad*");
+    const soyadInput = screen.getByLabelText("Soyad*");
+    const emailInput = screen.getByLabelText("Email*");
 
+    expect(adInput).toBeInTheDocument();
+    expect(soyadInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+
+    const errorMessage = screen.queryAllByTestId("error");
+    expect(errorMessage).toHaveLength(0);
+  });
+
+  test("form gönderildiğinde girilen tüm değerler render ediliyor.", async () => {
+    const adInput = screen.getByLabelText("Ad*");
+    const soyadInput = screen.getByLabelText("Soyad*");
+    const emailInput = screen.getByLabelText("Email*");
+    const mesajInput = screen.getByLabelText("Mesaj");
+    const submitButton = screen.getByRole("button");
+
+    userEvent.type(adInput, "firstname");
+    userEvent.type(soyadInput, "lastname");
+    userEvent.type(emailInput, "test@email.com");
+    userEvent.type(mesajInput, "Hello World!");
+
+    fireEvent.click(submitButton);
+
+    expect(adInput).toHaveValue("firstname");
+    expect(soyadInput).toHaveValue("lastname");
+    expect(emailInput).toHaveValue("test@email.com");
+    expect(mesajInput).toHaveValue("Hello World!");
+  });
 });
